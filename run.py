34223906fea45ad89bac6406a2290f35c9e70493
Hannes Hellmér <hhellmer@gmail.com>
2023-04-12 06:10:26 +0000

    Add a print rules function

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
 import random
 """import random module for number generation"""
+
+
+# Welcome message and rules
+def print_rules():
+    """Function to print out the rules"""
+    print("Let's play Battleships!.")
+    print("You and the computer each have 3 ships.")
+    print("The computer's ships are hidden.")
+    print("Your ships are marked with a 1.")
+    print("You have to guess a row and column between 0-4 (0 indexing).")
+    print("You can not guess the same number combination twice.")
+
+print_rules()
